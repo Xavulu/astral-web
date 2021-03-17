6fed32b3a5f69dd08db8ee19d10f8b72d46bbde9
Xavier Fernandez <lain@MacBook-Pro-3.home>
2021-03-17 05:32:57 -0400

    im not much of a frontend developer so this probably doesn't work for now...

--- astral-place/App.ts
+++ astral-place/App.ts
@@ -1,6 +1,36 @@
 import * as astral_wasm from '@xavulu/astral_wasm'; 
 import * as peerjs from 'peerjs'; 
+import { encrypt_data } from '../pkg/astral_wasm_bg.wasm';
+
+const template: string = `
+<h1>WebAssembly - Rust + TypeScript</h1>
+<span></span>
+<p></p>
+`;
+
 
 export class App extends HTMLElement {
-     
-}
+    public static TAG: string = `test-wasm`;
+    private data: Uint8Array = new Uint8Array([0, 2, 23, 90, 12, 17]);
+    private password: string = "badpass";
+
+    public readonly shadowRoot!: ShadowRoot;
+    public readonly expression: HTMLSpanElement;
+    public readonly score: HTMLParagraphElement; 
+    private encrypted: Uint8Array; 
+    private decrypted: Uint8Array;
+
+    constructor(){
+        super(); 
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = template;
+        this.expression = this.shadowRoot.querySelector('span')!;
+        this.score = this.shadowRoot.querySelector('p')!;
+        this.encrypted = new Uint8Array(astral_wasm.encrypt_data(this.data, this.password, true));
+        this.decrypted = new Uint8Array(astral_wasm.decrypt_data(this.data, this.password)); 
+        this.expression.textContent = `${this.encrypted} -> ${this.decrypted}`
+
+    }
+    
+} 
+customElements.define(App.TAG, App);
